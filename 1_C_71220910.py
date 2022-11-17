c2bdb50edc8860a3384ba47d380aac50e45dee8b
dianzebua <117893767+dianzebua@users.noreply.github.com>
2022-11-17 16:30:38 +0700

    Add files via upload

---
+++ 1_C_71220910.py
@@ -0,0 +1,10 @@
+namadepan = "Cassandra"
+namabelakang = "Charlotte"
+matakuliah = "Logika Matematika"
+grup = "A"
+input ("Masukkan nama Anda : Cassandra Charlotte")
+input ("Masukkan mata kuliah : Logika Matematika")
+input ("Masukkan grup : A")
+print ("Hallo", namabelakang)
+print (namabelakang, "tergabung dalam kelas", matakuliah, "pada grup", grup)
+
